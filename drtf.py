627f936fc818ce9de52a95b4682a06585f44c3ff
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2023-07-06 21:37:23 +0400

    update25

--- drtf.py
+++ drtf.py
@@ -632,14 +632,14 @@ class Stack(nn.Module):
 		for block_id in range(len(self.blocks)):
 			b, f = self.blocks[block_id](backcast)
 			#backcast = torch.cat( [(backcast[:,:,0] - b).view([-1,self.backcast_length,1]),backcast[:,:,1:] ],dim=2)
-			if len(backcast) == 0:
-				backcast = torch.zeros([BATCHSIZE, self.backcast_length, nv]).to(self.device)
+			print('b')
+			print(b)
+			print(type(b))
+			print(b.shape)
+			print('---------------------------------------------------')
+			# if len(backcast) == 0:
+			# 	backcast = torch.zeros([BATCHSIZE, self.backcast_length, nv]).to(self.device)
 			if AVD:
-				print('backcast.clone()')
-				print(backcast.clone())
-				print(type(backcast.clone()))
-				print(backcast.clone().shape)
-				print('---------------------------------------------------')
 				backtargs.append(backcast.clone()[:,:,0])
 				backcast2=backcast.clone()
 				backcast2[:,:,0]=backcast2[:,:,0]-b
